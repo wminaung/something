ffdc669961fd4cb1608852b79b729231d7485eef
wminaung <minwin243@gmail.com>
2024-02-01 20:16:51 +0630

    prisma deploy on vercel

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,15 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
     console.log(process.env.POSTGRES_URL);
-    // const result = await prisma.user.findMany();
-    const result = { hello: "World" };
-    // const result = await prisma.user.create({
-    //   data: {
-    //     email: "minwin243@gmail.com",
-    //     name: "Win Min Aung",
-    //   },
-    // });
-    return NextResponse.json({ result }, { status: 200 });
+    const result = await prisma.user.findMany();
+
+    return NextResponse.json({ users: result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
